0748390c9fce35005481b36ca0620ac3b9a3fa68
monaeman <mona.eman2018@gmail.com>
2023-07-21 08:11:11 -0400

    added codes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Arrays from "./Arrays";
+import Objects from "./object";
+import { useState } from "react";
+import Form from "./Form";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  const handleDecrement = () => {};
+
+  const handleIncrementFive = () => {
+    setCounter(counter + 5);
+  };
+  const handleDecrementFive = () => {
+    setCounter(counter - 5);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Drills</h1>
+      {/* <Arrays /> */}
+      {/* <Objects /> */}
+      <h1> {counter} </h1>
+      <button onClick={handleIncrement}> + </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        {" "}
+        -{" "}
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          setCounter(0);
+        }}
+      >
+        reset
+      </button>
+      <button onClick={handleIncrementFive}> Increment +5</button>
+      <button onClick={handleDecrementFive}> Decrement -5</button>
+      <h1>Form</h1>
+      <Form />
     </div>
   );
 }
